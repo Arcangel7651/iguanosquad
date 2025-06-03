03c6e4ea5e72b4019cdfc1cfe1101975bfd4fa2b
Mayela35 <126261671+Mayela35@users.noreply.github.com>
2025-06-03 09:39:05 -0600

    caso de uso 11

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -1,6 +1,7 @@
 // lib/widgets/event_card.dart
 
 import 'package:flutter/material.dart';
+import 'package:iguanosquad/services/actividad_service.dart';
 import 'package:path/path.dart' as path; // si lo necesitas más adelante
 import '../screens/edit_event_screen.dart';
 
@@ -9,8 +10,7 @@ class EventCard extends StatelessWidget {
   final DateTime date;
   final String location;
   final String description;
-  final int participants;
-  final bool isActive;
+  final int id;
   final String imageURL;
   final VoidCallback? onEdit;
   final VoidCallback? onDelete;
@@ -21,8 +21,7 @@ class EventCard extends StatelessWidget {
     required this.date,
     required this.location,
     required this.description,
-    required this.participants,
-    this.isActive = true,
+    required this.id,
     this.onEdit,
     this.onDelete,
     required this.imageURL,
@@ -32,7 +31,7 @@ class EventCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final formattedDate =
         '${date.day} de ${_spanishMonth(date.month)}, ${date.year}';
-
+    final isActive = !hasDatePassed();
     return Card(
       color: Colors.white,
       margin: const EdgeInsets.only(bottom: 16),
@@ -102,8 +101,49 @@ class EventCard extends StatelessWidget {
                         );
                       },
                     ),
-                    // si quieres onDelete:
-                    // IconButton(icon: Icon(Icons.delete_outline), onPressed: onDelete),
+                    IconButton(
+                      icon: const Icon(Icons.delete_outline),
+                      onPressed: () async {
+                        final confirmed = await showDialog<bool>(
+                          context: context,
+                          builder: (ctx) => AlertDialog(
+                            title: const Text('Confirmar'),
+                            content: const Text(
+                                '¿Seguro que quieres eliminar esta actividad?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.pop(ctx, false),
+                                child: const Text('Cancelar'),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.pop(ctx, true),
+                                child: const Text('Eliminar'),
+                              ),
+                            ],
+                          ),
+                        );
+
+                        if (confirmed == true) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Actividad eliminada'),
+                              duration: Duration(seconds: 5),
+                            ),
+                          );
+                          final success = await ActivityService()
+                              .eliminarActividadPorId(id);
+
+                          if (success) {
+                            // Luego verifica si el context aún está montado antes de usarlo
+                            if (!context.mounted) {
+                              return;
+                            }
+
+                            onDelete!(); //Esto reincia la pag
+                          }
+                        }
+                      },
+                    ),
                   ],
                 ),
                 const SizedBox(height: 8),
@@ -142,26 +182,59 @@ class EventCard extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      children: [
-                        Icon(Icons.people, size: 16, color: Colors.grey[600]),
-                        const SizedBox(width: 4),
-                        Text('$participants participantes',
-                            style: TextStyle(color: Colors.grey[600])),
-                      ],
+                    FutureBuilder<int>(
+                      future: _loadParticipants(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Row(
+                            children: [
+                              Icon(Icons.people,
+                                  size: 16, color: Colors.grey[600]),
+                              const SizedBox(width: 4),
+                              Text('Cargando participantes...',
+                                  style: TextStyle(color: Colors.grey[600])),
+                            ],
+                          );
+                        } else if (snapshot.hasError) {
+                          return Row(
+                            children: [
+                              Icon(Icons.people,
+                                  size: 16, color: Colors.grey[600]),
+                              const SizedBox(width: 4),
+                              Text('Error',
+                                  style: TextStyle(color: Colors.red)),
+                            ],
+                          );
+                        } else {
+                          final participants = snapshot.data ?? 0;
+                          return Row(
+                            children: [
+                              Icon(Icons.people,
+                                  size: 16, color: Colors.grey[600]),
+                              const SizedBox(width: 4),
+                              Text('$participants participantes',
+                                  style: TextStyle(color: Colors.grey[600])),
+                            ],
+                          );
+                        }
+                      },
                     ),
                     Container(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 8, vertical: 4),
                       decoration: BoxDecoration(
-                        color: isActive ? Colors.green[100] : Colors.grey[200],
+                        color: isActive
+                            ? Colors.green[100]
+                            : const Color.fromARGB(255, 240, 158, 158),
                         borderRadius: BorderRadius.circular(4),
                       ),
                       child: Text(
                         isActive ? 'Activo' : 'Inactivo',
                         style: TextStyle(
-                          color:
-                              isActive ? Colors.green[800] : Colors.grey[600],
+                          color: isActive
+                              ? Colors.green[800]
+                              : const Color.fromARGB(255, 255, 45, 45),
                           fontSize: 12,
                         ),
                       ),
@@ -193,5 +266,18 @@ class EventCard extends StatelessWidget {
       'diciembre'
     ];
     return meses[month];
+  }
+
+  bool hasDatePassed() {
+    final now = DateTime.now();
+    final eventDate = DateTime(date.year, date.month, date.day);
+    final today = DateTime(now.year, now.month, now.day);
+    return eventDate.isBefore(today);
+  }
+
+  Future<int> _loadParticipants() async {
+    // Simula la llamada al servicio asíncrono
+    final int count = await ActivityService().countParticipants(id);
+    return count;
   }
 }
